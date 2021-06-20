7c2e7ae06bfc7d6631d11139831348e0f161927c
SumanIslam <sumanislam@gmail.com>
2021-06-20 20:33:54 +0600

    delete data from database

--- main.js
+++ main.js
@@ -7,15 +7,24 @@ const renderCafe = (doc) => {
   let li = document.createElement('li');
   let name = document.createElement('span');
   let city = document.createElement('span');
+  let cross = document.createElement('div');
 
   li.setAttribute('data-id', doc.id);
-  name.textContent= doc.data().name;
+  name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  cross.textContent = 'X';
 
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(cross);
 
   cafeList.appendChild(li);
+
+  cross.addEventListener('click', (e) => {
+    e.preventDefault();
+    let id = e.target.parentElement.getAttribute('data-id');
+    firestore.collection('cafes').doc(id).delete();
+  })
 }
 
 // getting data
