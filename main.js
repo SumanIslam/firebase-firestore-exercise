60c14fbe50b740c77c8359a9fb758e6469c2081a
SumanIslam <sumanislam@gmail.com>
2021-06-20 23:10:11 +0600

    added realtime listener functionality

--- main.js
+++ main.js
@@ -27,12 +27,12 @@ const renderCafe = (doc) => {
   })
 }
 
-// getting data
-firestore.collection('cafes').get().then((snapshot) => {
-  snapshot.docs.forEach(doc => {
-    renderCafe(doc);
-  })
-})
+// // getting data
+// firestore.collection('cafes').get().then((snapshot) => {
+//   snapshot.docs.forEach(doc => {
+//     renderCafe(doc);
+//   })
+// })
 
 // saving data
 form.addEventListener('submit', (e) => {
@@ -43,4 +43,17 @@ form.addEventListener('submit', (e) => {
   });
   form.name.value = '';
   form.city.value = '';
+})
+
+// realtime listener
+firestore.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+  const changes = snapshot.docChanges();
+  changes.forEach(change => {
+    if(change.type == 'added') {
+    renderCafe(change.doc);
+    } else if(change.type == 'removed') {
+      let li = cafeList.querySelector(`[data-id=${change.doc.id}]`);
+      cafeList.removeChild(li);
+    }
+  })
 })
